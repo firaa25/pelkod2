d1efa9c84e1eed5ce432529cdbd925965f02817e
firaa25 <143860017+firaa25@users.noreply.github.com>
2023-09-02 10:59:34 +0700

    Add files via upload
    upload file

---
+++ latihan1.py
@@ -0,0 +1,41 @@
+nama_saya = "Nama saya fira"
+
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = 21
+
+print(umur, "bertipe", type(umur))
+
+ipk = 4.00
+
+print(ipk, "bertipe", type(ipk))
+
+menikah = False
+
+print("menikah?",menikah, "bertipe", type(menikah))
+
+matkul_diambil =["alpro", "sig"]
+print("matkul : ",matkul_diambil, "bertipe", type(matkul_diambil))
+
+matkul_diambil2 =("web", "pemvis")
+print("matkul lain : ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+matkul_diambil3 ={"alpro", "aLPRO", "pemweb","data science", "data science"}
+print("matkul lain-lain : ",matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+print(15 / 3)
+print(15 // 3)
+print(15 % 3)
+
+nama_universitas = "universitas trunojoyo"
+print(nama_universitas)
+
+nama_universitas = str(input("masukan nama universitas="))
+print(nama_universitas)
+
+angka1 = int(input("masukan angka 1 = "))
+angka2 = int(input("masukan angka 2 = "))
+print(angka1+angka2)
+
+
+
